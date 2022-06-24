419c595560106f01052328cb419519dbdc147e5c
Esteban Bosques <nano.95.eb@gmail.com>
2022-06-24 16:33:39 -0600

    select position in progress bar functionality added

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const image = document.querySelector("img");
 const title = document.getElementById("title");
 const artist = document.getElementById("artist");
 const music = document.querySelector("audio");
+const progressContainer = document.getElementById("progress-container");
+const progress = document.getElementById("progress");
+const currentTimeEl = document.getElementById("current-time");
+const durationEl = document.getElementById("duration");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
@@ -74,7 +78,7 @@ function prevSong() {
 
 // Next Song
 function nextSong() {
-  songIndex = songIndex === songs.length ? 0 : songIndex + 1;
+  songIndex = songIndex === songs.length - 1 ? 0 : songIndex + 1;
   loadSong(songs[songIndex]);
   playSong();
 }
@@ -82,6 +86,47 @@ function nextSong() {
 // On load - Select first song
 loadSong(songs[songIndex]);
 
+function parseTime(time) {
+  // Calculate display for duration
+  const timeMin = Math.floor(time / 60);
+  let timeSec = Math.floor(time % 60);
+
+  if (timeSec < 10) {
+    timeSec = `0${timeSec}`;
+  }
+
+  return `${timeMin}:${timeSec}`;
+}
+
+// Update Progress Bar & Time
+function updateProgressBar(e) {
+  if (isPlaying) {
+    const { duration, currentTime } = e.srcElement;
+
+    if (duration) {
+      // Update progress bar width
+      const progessPercent = (currentTime / duration) * 100;
+      progress.style.width = `${progessPercent}%`;
+
+      // Update time text
+      durationEl.textContent = parseTime(duration);
+      currentTimeEl.textContent = parseTime(currentTime);
+    }
+  }
+}
+
+// Set Progress Bar
+function setProgressBar(e) {
+  const width = this.clientWidth;
+  const clickPosition = e.offsetX;
+
+  const { duration } = music;
+  music.currentTime = (clickPosition / width) * duration;
+}
+
 // Event Listeners
 prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
+music.addEventListener("timeupdate", updateProgressBar);
+music.addEventListener("ended", nextSong);
+progressContainer.addEventListener("click", setProgressBar);
